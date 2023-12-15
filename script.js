cb1a6b5a2cd2cf145f51c6a25e8c5bae8a916fee
Rose <rosebeats09@gmail.com>
2023-12-15 18:57:42 -0500

    Delete ideas gif since its not working

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 // eslint-disable-next-line max-classes-per-file
 class Node {
-  constructor(location) {
-    this.location = location;
+  constructor(position) {
+    this.position = position;
     this.children = [];
-    this.parents = null;
+    this.parent = null;
   }
   // do not allow any moves to go off the board
 }
 class Knight {
   constructor(startingSquare, endingSquare) {
-    this.root = null;
     this.startingSquare = startingSquare;
+    // 8 possible moves
     this.moves = [
       [1, 2],
       [1, -2],
       [-1, 2],
       [-1, -2],
-      [-1, -1],
       [2, 1],
       [2, -1],
       [-2, 1],
@@ -32,8 +31,18 @@ class Knight {
 //  [4,5]
 //  [2,4]
 //  [4,3]
+
+const gameBoard = () => {
+  let columns = 8;
+  let rows = 8;
+  for (let i = 0; i<)
+}
+const knight1 = new Knight(startingSquare, endingSquare);
+console.log(knight1);
+
 function knightMoves(startingSquare, endingSquare) {
-  const knight1 = new Knight(startingSquare, endingSquare);
-  console.log(knight1);
+  const visited = [];
+  const queue = [];
+  let enqueue = 
 }
 knightMoves[3, 3], [4, 3];
